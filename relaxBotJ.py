0243a9e1cb0fe401bf2e90e66a136bd603f25c86
Scepter03 <JMCal30@yahoo.com>
2020-07-04 11:42:33 -0400

    added stuff

--- relaxBotJ.py
+++ relaxBotJ.py
@@ -2,6 +2,14 @@
 import os
 import discord
 
+@client.event
+async def on_message(message):
+    message.content.lower()
+    if message.author == client.user:
+        return
+    if message.content.startswith("hello"):
+        await message.channel.send("Hello, I am a test bot.")
+
 client = discord.Client()
 
 client.run('NzI4NzY5NTQ5NjUwMTAwMjY1.XwCiWQ.VTmc5_t6aPcCAxmeYbXuz71C-Lg')
