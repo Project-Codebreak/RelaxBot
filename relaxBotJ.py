7e455be193018433412838379bc2e1a0c9c4a917
Scepter03 <JMCal30@yahoo.com>
2020-07-04 12:16:43 -0400

    added me

--- relaxBotJ.py
+++ relaxBotJ.py
@@ -13,6 +13,8 @@ async def on_message(message):
         await message.channel.send("Hello, I am a test bot.")
     if message.author == "Wessymessy#1893":
         await message.channel.send("No. Go Away.")
+    if message.author == "Raging_Beast#4944":
+        await message.channel.send("Hi! You're Awesome!")
 
 
 client.run('NzI4NzY5NTQ5NjUwMTAwMjY1.XwCiWQ.VTmc5_t6aPcCAxmeYbXuz71C-Lg')
