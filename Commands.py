2084a87582116caf06c8fe576951e578dea1c920
Scepter03 <JMCal30@yahoo.com>
2020-07-04 16:54:03 -0400

    edited

--- Commands.py
+++ Commands.py
@@ -9,7 +9,6 @@ import os
 TOKEN = 'NzI4NzY5NTQ5NjUwMTAwMjY1.XwCiWQ.VTmc5_t6aPcCAxmeYbXuz71C-Lg'
 client = commands.Bot(command_prefix = '.')
 
-
 @client.event
 async def on_ready():
     print('The bot is ready to start the vibes :)')
@@ -28,7 +27,16 @@ async def join(ctx):
 
 @client.command()
 async def leave(ctx):
-    await ctx.voice_client.disconnect()
+    channel = ctx.message.author.voice.channel
+    voice = get(client.voice_client, guild=ctx.guild)
+
+    if voice and voice.is_connected():
+        await voice.disconnect()
+        print(f"The bot has left {channel}")
+        await ctx.send(f"Left {channel}")
+    else:
+        print("Bot was told to leave voice channel, but was not in one")
+        await ctx.send("Don't think I am in a voice channel")
 
 
 client.run(TOKEN)
