fa873ea6c9149d5a53e8164c6e182a83fff8f3f2
Scepter03 <JMCal30@yahoo.com>
2020-07-04 13:46:44 -0400

    commands

--- relaxBotJ.py
+++ relaxBotJ.py
@@ -12,5 +12,9 @@ async def on_message(message):
     if message.content.startswith("hello"):
         await message.channel.send("Hello, I am a test bot.")
 
+@client.command()
+async def ping(ctx):
+    await ctx.send('Pong!') 
+
 client.run('NzI4NzY5NTQ5NjUwMTAwMjY1.XwCiWQ.VTmc5_t6aPcCAxmeYbXuz71C-Lg')
 
