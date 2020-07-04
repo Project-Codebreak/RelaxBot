96c5901f6b0e6ae3bd9adf42a81b258b15b2fc31
Scepter03 <JMCal30@yahoo.com>
2020-07-04 13:55:20 -0400

    s

--- relaxBotJ.py
+++ relaxBotJ.py
@@ -2,9 +2,7 @@
 import discord
 from discord.ext import commands
 
-client = discord.Client()
-
-userCommands = commands.Bot(command_prefix= '~')
+client = commands.Bot(command_prefix= '~')
 
 @client.event
 async def on_ready():
@@ -16,7 +14,7 @@ async def on_message(message):
     if message.content.startswith("hello"):
         await message.channel.send("Hello, I am a test bot.")
 
-@userCommands.command()
+@client.command()
 async def ping(ctx):
     await ctx.send(f'Pong! {round(client.latency * 1000)}ms')
 
