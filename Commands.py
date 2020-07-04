78d55708f0ce27759d61b90c27ee6bafc4f64abb
Scepter03 <JMCal30@yahoo.com>
2020-07-04 17:35:43 -0400

    changed join and leave

--- Commands.py
+++ Commands.py
@@ -9,34 +9,18 @@ import os
 TOKEN = 'NzI4NzY5NTQ5NjUwMTAwMjY1.XwCiWQ.VTmc5_t6aPcCAxmeYbXuz71C-Lg'
 client = commands.Bot(command_prefix = '.')
 
+
 @client.event
 async def on_ready():
     print('The bot is ready to start the vibes :)')
 
 @client.command()
 async def join(ctx):
-    channel = ctx.message.author.voice.channel
-    voice = get(client.voice_client, guild=ctx.guild)
+    async def join(ctx):
+        channel = ctx.message.author.voice.channel
+        await client.join_voice_channel(channel)
 
-    if voice and voice.is_connected():
-        await voice.move_to(channel)
-    else:
-        voice = await channel.connect()
-        print(f"The bot has connected to {channel}\n")
-    await ctx.send(f"Joined {channel}")
-
-@client.command()
-async def leave(ctx):
-    channel = ctx.message.author.voice.channel
-    voice = get(client.voice_client, guild=ctx.guild)
-
-    if voice and voice.is_connected():
-        await voice.disconnect()
-        print(f"The bot has left {channel}")
-        await ctx.send(f"Left {channel}")
-    else:
-        print("Bot was told to leave voice channel, but was not in one")
-        await ctx.send("Don't think I am in a voice channel")
+    print('Bot joined the channel.')
 
 
 client.run(TOKEN)
