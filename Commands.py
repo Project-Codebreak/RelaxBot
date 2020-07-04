b91e34e2968e495b4677e9381239bbdc035f1260
Scepter03 <JMCal30@yahoo.com>
2020-07-04 16:27:06 -0400

    changes

--- Commands.py
+++ Commands.py
@@ -6,6 +6,7 @@ from discord.ext import commands
 TOKEN = 'NzI4NzY5NTQ5NjUwMTAwMjY1.XwCiWQ.VTmc5_t6aPcCAxmeYbXuz71C-Lg'
 client = commands.Bot(command_prefix = '.')
 
+
 @client.event
 async def on_ready():
     print('The bot is ready to start the vibes :)')
@@ -14,9 +15,7 @@ async def on_ready():
 async def join(ctx):
     channel = ctx.author.voice.channel
     await channel.connect()
-
 @client.command()
 async def leave(ctx):
     await ctx.voice_client.disconnect()
-client.run(TOKEN)
 
