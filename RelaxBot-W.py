7f59113a294ccabcb7c467c4bcfc2708c6273126
Weesal <wesley.cadiz@gmail.com>
2020-07-04 13:16:19 -0700

    My code thingy

--- RelaxBot-W.py
+++ RelaxBot-W.py
@@ -1,17 +1,39 @@
 import discord
+import random
+from discord.ext import commands
 
-client = discord.Client()
+client = commands.Bot(command_prefix = '.')
 
 @client.event
-async def on_message(message):
-    message.content = message.content.lower()
-    if message.author == client.user:
-        return
-    if message.content.startswith("hello"):
-        if str(message.author) == "Wessymessy#1893":
-            await message.channel.send("No. Go Away.")
-        else:
-            await message.channel.send("Hello, I am a test bot.")
+async def on_ready():
+    print("Bot is ready.")
 
+@client.command()
+async def ping(ctx):
+    await ctx.send(f"Pong! {round (client.latency * 1000)}ms")
 
-client.run('NzI4NzY5NTQ5NjUwMTAwMjY1.XwCiWQ.VTmc5_t6aPcCAxmeYbXuz71C-Lg')
+@client.command(aliases=['8ball'])
+async def _8ball(ctx, *, question):
+    responses = ['It is certain.',
+                'It is decidedly so.',
+                'Without a doubt.',
+                'Yes – definitely.',
+                'You may rely on it.',
+                'As I see it, yes.',
+                'Most likely.',
+                'Outlook good.',
+                'Yes.',
+                'Signs point to yes.',
+                'Reply hazy, try again.',
+                'Ask again later.',
+                'Better not tell you now.',
+                'Cannot predict now.',
+                'Concentrate and ask again.',
+                'Do not count on it.',
+                'My reply is no.',
+                'My sources say no.',
+                'Outlook not so good.',
+                'Very doubtful.']
+    await ctx.send(f'Question: {question}\nAnswer: {random.choice(responses)}')
+    
+client.run('NzI5MDExODcyOTU0NzEyMTM0.XwC8JA.jcSJZmFrRSIT76S07AM-R0xYdw4')
