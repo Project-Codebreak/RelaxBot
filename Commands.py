c7133929a1fc39520d4680f8cfd2c6d98a358f8c
Scepter03 <JMCal30@yahoo.com>
2020-07-04 22:04:02 -0400

    Added join and leave function

--- Commands.py
+++ Commands.py
@@ -4,11 +4,9 @@ from discord.ext import commands
 from discord.utils import get
 import random
 import os
-from pip import pynacl
 
 TOKEN = 'NzI4NzY5NTQ5NjUwMTAwMjY1.XwCiWQ.VTmc5_t6aPcCAxmeYbXuz71C-Lg'
-client = commands.Bot(command_prefix = '.')
-
+client = commands.Bot(command_prefix = '$')
 
 @client.event
 async def on_ready():
@@ -16,10 +14,20 @@ async def on_ready():
 
 @client.command()
 async def join(ctx):
-     channel = ctx.message.author.voice.channel
-     await channel.connect()
-     print('Bot joined the channel.')
+    channel = ctx.message.author.voice.channel
+    voice = get(client.voice_clients, guild=ctx.guild)
+    
 
+    if voice and voice.is_connected:
+        await ctx.send(f"Sorry. The bot is currently in use in another voice channel")
+    else:
+        await channel.connect()
+        print('Bot joined the channel.')
 
+@client.command()
+async def leave(ctx):
+    channel = ctx.message.guild.voice_client
+    await channel.disconnect()
+    print('Bot lefted the channel.')
 client.run(TOKEN)
 
